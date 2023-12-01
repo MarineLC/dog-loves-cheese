06f7189fcffaf3963fc3ede50380df028ad4623e
Marine Le Cleach <lecleach.m@gmail.com>
2023-12-01 16:44:30 +0100

    second screen

--- js/game.js
+++ js/game.js
@@ -3,13 +3,23 @@ class Game{
     constructor(){
 
         this.startScreen = document.getElementById('start-screen');
+        this.heaDog = document.getElementById('head-dog');
         this.gameScreen = document.getElementById('game-start');
         this.gameEndScreen = document.getElementById('game-end');
+
+        this.height = '600';
+        this.width = '1000';
+
+
     }
 
 
     launch(){
+        this.gameScreen.style.height = `${this.height}px`;
+        this.gameScreen.style.width = `${this.width}px`;
         this.startScreen.style.display = 'none';
+        this.heaDog.style.display = 'none';
+        console.log(this.startScreen.style.display);
         this.gameScreen.style.display = 'block';
     }
 
